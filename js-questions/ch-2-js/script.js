2a5f23801b19c292982347ca7d349409fc67c19b
Rafi-Rafique <160730669+Rafi-Rafique@users.noreply.github.com>
2024-05-11 00:35:03 +0500

    Add files via upload

---
+++ js-questions/ch-2-js/script.js
@@ -0,0 +1,21 @@
+var myName = "Rafia Rafique";
+alert(myName);
+var jS = "Hello World";
+alert(jS);
+var yourAge = "27 years old";
+alert(yourAge);
+var yourCourse = "Certified Mobile Application Development";
+alert(yourCourse);
+
+
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+var yourEmail = "rafi.rafiquepk@gmail.com" ;
+alert("my email address is " + yourEmail);
+
+var myBook = "A smarter way to learn javascript";
+alert("I am trying to learn From the book " + myBook);
+
+
+var endDesign = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(endDesign);
---
+++ js-questions/ch-3-js/script.js
@@ -0,0 +1,13 @@
+var age = "27 years old";
+alert(age);
+var visit = "14 times";
+alert("You have visited this site " + visit);
+
+var birthYear = 1996;
+alert(birthYear);
+
+var type = "Data type of my declared variable is ";
+alert(type + typeof birthYear);
+
+document.writeln("My birth year is 1996 "); 
+document.writeln("\n Data type of my declared variable is number")
